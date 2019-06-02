7feb9e0df6348bb58015aef037b9eaed98e4db33
Carey Hinoki <carey.hinoki@gmail.com>
2019-06-02 02:08:53 -0700

    build(*): Update gatsby default metadata.

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,12 +18,12 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'healing-in-therapy',
+        short_name: 'healing',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
-        display: 'minimal-ui',
+        display: 'standalone',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
