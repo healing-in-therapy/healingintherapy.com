d4e37eb76c59742ca07171dc765e30a4cc6b230f
Carey Hinoki <carey.hinoki@gmail.com>
2019-06-09 23:58:01 -0700

    feat(contact): Add netlify form.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 
 import {
   Column,
@@ -8,20 +8,208 @@ import {
   Section,
 } from 'components';
 
+import styles from './style/contact.module.scss';
+
 function ContactPage() {
+  const [interest, setInterest] = useState(null);
+
+  const email = useRef(null);
+  const form = useRef(null);
+  const message = useRef(null);
+  const name = useRef(null);
+  const phone = useRef(null);
+
+  function onInterestChange(event) {
+    setInterest(event.target.value);
+  }
+
+  const onSubmit = useCallback((event) => {
+    event.preventDefault();
+
+    const data = {
+      interest,
+      email: email.current.value,
+      message: message.current.value,
+      name: name.current.value,
+      phone: phone.current.value,
+      'form-name': form.current.name,
+    };
+
+    const body = Object.keys(data)
+      .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+      .join('&')
+    ;
+
+    fetch('/', {
+      body,
+      method: 'POST',
+
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+
+    form.current.reset();
+  }, [interest])
+
   return (
     <Layout title="Contact">
       <h1>Contact</h1>
 
-      <Row>
-        <Column width="50%">
-          <Map />
-        </Column>
+      <Section>
+        <Row>
+          <Column width="50%">
+            <form
+              data-netlify-honeypot="bot-field"
+              data-netlify="true"
+              id="contact_form"
+              method="post"
+              name="contact"
+              onSubmit={onSubmit}
+              ref={form}
+            >
+              <input type="hidden" name="form-name" value="contact" />
 
-        <Column width="50%">
-          <Section
-            title="My Office"
-          >
+              <div className={styles.honeypot}>
+                <input
+                  name="bot-field"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="contact_form_name">Name</label>
+                <input
+                  id="contact_form_name"
+                  name="name"
+                  ref={name}
+                  required
+                  type="text"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="contact_form_email">Email</label>
+                <input
+                  id="contact_form_email"
+                  name="email"
+                  ref={email}
+                  required
+                  type="email"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="contact_form_phone">Phone (optional)</label>
+                <input
+                  id="contact_form_phone"
+                  name="phone"
+                  ref={phone}
+                  type="tel"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="contact_form_message">Message</label>
+                <textarea
+                  id="contact_form_message"
+                  name="message"
+                  ref={message}
+                  required
+                />
+              </div>
+
+              <div>
+                Interest
+
+                <ul>
+                  <li>
+                    <label>
+                      <input
+                        id="contact_form_interest"
+                        name="interest"
+                        onChange={onInterestChange}
+                        required
+                        type="radio"
+                        value="individuals"
+                      />
+
+                      Individuals
+                    </label>
+                  </li>
+
+                  <li>
+                    <label>
+                      <input
+                        id="contact_form_interest"
+                        name="interest"
+                        onChange={onInterestChange}
+                        required
+                        type="radio"
+                        value="couples"
+                      />
+
+                      Couples
+                    </label>
+                  </li>
+
+                  <li>
+                    <label>
+                      <input
+                        id="contact_form_interest"
+                        name="interest"
+                        onChange={onInterestChange}
+                        required
+                        type="radio"
+                        value="groups"
+                      />
+
+                      Groups
+                    </label>
+                  </li>
+
+                  <li>
+                    <label>
+                      <input
+                        id="contact_form_interest"
+                        name="interest"
+                        onChange={onInterestChange}
+                        required
+                        type="radio"
+                        value="rehabilitation"
+                      />
+
+                      Rehabilitation
+                    </label>
+                  </li>
+
+                  <li>
+                    <label>
+                      <input
+                        id="contact_form_interest"
+                        name="interest"
+                        onChange={onInterestChange}
+                        required
+                        type="radio"
+                        value="other"
+                      />
+
+                      Other
+                    </label>
+                  </li>
+                </ul>
+              </div>
+
+              <div>
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+          </Column>
+
+          <Column width="50%">
+            <Map />
+
+            <h2>My Office</h2>
+
             <address>
             1801 Park Court Pl. Unit E 105<br />
             Santa Ana, CA 92701
@@ -32,80 +220,9 @@ function ContactPage() {
             <p>
               There is plently of free parking available in front of the building.
             </p>
-          </Section>
-        </Column>
-      </Row>
-
-      <Row>
-        <Column>
-          <form id="contact_form" name="contact_form">
-            <div>
-              <label htmlFor="contact_form_name">Name</label>
-              <input id="contact_form_name" name="name" type="text" />
-            </div>
-
-            <div>
-              <label htmlFor="contact_form_email">Email</label>
-              <input id="contact_form_email" name="email" type="email" />
-            </div>
-
-            <div>
-              <label htmlFor="contact_form_phone">Phone (optional)</label>
-              <input id="contact_form_phone" name="phone" type="text" />
-            </div>
-
-            <div>
-              <label htmlFor="contact_form_message">Message</label>
-              <textarea id="contact_form_message" name="message" />
-            </div>
-
-            <div>
-              Interest
-
-              <ul>
-                <li>
-                  <label>
-                    <input id="contact_form_interest" name="interest" type="radio" value="individuals" />
-                    Individuals
-                  </label>
-                </li>
-
-                <li>
-                  <label>
-                    <input id="contact_form_interest" name="interest" type="radio" value="couples" />
-                    Couples
-                  </label>
-                </li>
-
-                <li>
-                  <label>
-                    <input id="contact_form_interest" name="interest" type="radio" value="groups" />
-                    Groups
-                  </label>
-                </li>
-
-                <li>
-                  <label>
-                    <input id="contact_form_interest" name="interest" type="radio" value="rehabilitation" />
-                    Rehabilitation
-                  </label>
-                </li>
-
-                <li>
-                  <label>
-                    <input id="contact_form_interest" name="interest" type="radio" value="other" />
-                    Other
-                  </label>
-                </li>
-              </ul>
-            </div>
-
-            <div>
-              <button type="submit">Submit</button>
-            </div>
-          </form>
-        </Column>
-      </Row>
+          </Column>
+        </Row>
+      </Section>
     </Layout>
   );
 }
